02d9fb2578e636889c09045fb158dd589988aa61
JunkingSoul <2414212886@qq.com>
2016-03-04 15:52:18 +0800

    eww
    eefsefwefwf

---
+++ giii/giii/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  giii
+//
+//  Created by jiangxing on 16/3/4.
+//  Copyright © 2016年 jiangxing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ giii/giii/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  giii
+//
+//  Created by jiangxing on 16/3/4.
+//  Copyright © 2016年 jiangxing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
